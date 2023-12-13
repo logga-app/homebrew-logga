0a9bfc0651b48d59b58cb1f5820a7a761a47f470
nagypeterjob <nagypeterjob@gmail.com>
2023-12-13 22:34:57 +0000

    chore: add version 1.0.5

--- Casks/logga.rb
+++ Casks/logga.rb
@@ -1,27 +1,34 @@
-cask "logga" do
-  version "1.0.3"
-  sha256 "0ddc7fc1f3871e46d1d9a994e5bd589fa161c5de6added4fe68f6dd444262726"
+cask "logga-daemon" do
+  version "1.0.5"
+  sha256 "ae81d6230113af8a22e4b4dfe4e54745008c5c95e3cd62ec8fb937f810eb3513  logga-daemon-1.0.5.pkg"
 
-  url "https://github.com/logga-app/logga-public/releases/download/#{version}/logga-client-#{version}.pkg",
-    verified: "github.com/logga-app/logga-public/releases/download/"
-  name "logga app"
+  url "https://storage.getlogga.com/logga-daemon-#{version}.pkg",
+    verified: "storage.getlogga.com"
+  name "logga daemon"
   desc "Audit logging on modern macOS"
   homepage "https://getlogga.com/"
 
   depends_on macos: ">= :ventura"
 
-  pkg "logga-client-#{version}.pkg"
-  uninstall pkgutil: "com.logga.client.*",
-            delete: "/Library/Application Support/Logga"
+  daemon_path = "/Library/LaunchDaemons/com.logga.client.daemon.service.plist"
+
+  pkg "logga-daemon-#{version}.pkg"
+  uninstall pkgutil: "com.logga.daemon.*",
+            delete: [
+              "/Library/Application Support/Logga",
+              daemon_path
+            ],
+            launchctl: [
+              "com.logga.client.daemon.service"
+            ]
 
   uninstall_preflight do
-    system_command "/Applications/logga.app/Contents/MacOS/logga", args: ["unload"], sudo: false
+      system_command "/bin/launchctl", args: ["unload", "-w", daemon_path], sudo: true
   end
-
+  
   zap trash: [
     "/Library/Application Support/Logga",
   ]
- 
-  caveats "To be able to use EndpointSecurity, " \
-  "#{token} must be granted Full Disk Access under System Preferences → Security & Privacy → Privacy"
+
+  caveats "To be able to use EndpointSecurity, "   "#{token} must be granted Full Disk Access under System Preferences → Security & Privacy → Privacy.\n\n"   "Starting the daemon: sudo launchctl load -w /Library/LaunchDaemons/com.logga.client.daemon.service.plist\n"   "Stopping the daemon: sudo launchctl load -w /Library/LaunchDaemons/com.logga.client.daemon.service.plist"
 end
