647a5b2acd8552a0a4c0d14cc0405498b8ce90bf
nagypeterjob <nagypeterjob@gmail.com>
2023-12-13 22:42:13 +0000

    chore: add version 1.0.6

--- Casks/logga-daemon.rb
+++ Casks/logga-daemon.rb
@@ -1,37 +1,34 @@
 cask "logga-daemon" do
-    version "1.0.3"
-    sha256 "dfe1dad17febcc879e1f60f7b578c497184907034f02c05eda349a701c910742"
-  
-    url "https://github.com/logga-app/logga-public/releases/download/#{version}/logga-daemon-#{version}.pkg",
-      verified: "github.com/logga-app/logga-public/releases/download/"
-    name "logga daemon"
-    desc "Audit logging on modern macOS"
-    homepage "https://getlogga.com/"
-  
-    depends_on macos: ">= :ventura"
-  
-    daemon_path = "/Library/LaunchDaemons/com.logga.client.daemon.service.plist"
+  version "1.0.6"
+  sha256 "2aab73a3691e561313b4b41a3dca4f8f25be6cb161c1676bc6a1b8f42ffb4ae9  logga-daemon-1.0.6.pkg"
 
-    pkg "logga-daemon-#{version}.pkg"
-    uninstall pkgutil: "com.logga.daemon.*",
-              delete: [
-                "/Library/Application Support/Logga",
-                daemon_path
-              ],
-              launchctl: [
-                "com.logga.client.daemon.service"
-              ]
+  url "https://storage.getlogga.com/logga-daemon-#{version}.pkg",
+    verified: "storage.getlogga.com"
+  name "logga daemon"
+  desc "Audit logging on modern macOS"
+  homepage "https://getlogga.com/"
 
-    uninstall_preflight do
-        system_command "/bin/launchctl", args: ["unload", "-w", daemon_path], sudo: true
-    end
-    
-    zap trash: [
-      "/Library/Application Support/Logga",
-    ]
-   
-    caveats "To be able to use EndpointSecurity, " \
-    "#{token} must be granted Full Disk Access under System Preferences → Security & Privacy → Privacy.\n\n" \
-    "Starting the daemon: sudo launchctl load -w /Library/LaunchDaemons/com.logga.client.daemon.service.plist\n" \
-    "Stopping the daemon: sudo launchctl load -w /Library/LaunchDaemons/com.logga.client.daemon.service.plist"
+  depends_on macos: ">= :ventura"
+
+  daemon_path = "/Library/LaunchDaemons/com.logga.client.daemon.service.plist"
+
+  pkg "logga-daemon-#{version}.pkg"
+  uninstall pkgutil: "com.logga.daemon.*",
+            delete: [
+              "/Library/Application Support/Logga",
+              daemon_path
+            ],
+            launchctl: [
+              "com.logga.client.daemon.service"
+            ]
+
+  uninstall_preflight do
+      system_command "/bin/launchctl", args: ["unload", "-w", daemon_path], sudo: true
   end
+  
+  zap trash: [
+    "/Library/Application Support/Logga",
+  ]
+
+  caveats "To be able to use EndpointSecurity, "   "#{token} must be granted Full Disk Access under System Preferences → Security & Privacy → Privacy.\n\n"   "Starting the daemon: sudo launchctl load -w /Library/LaunchDaemons/com.logga.client.daemon.service.plist\n"   "Stopping the daemon: sudo launchctl load -w /Library/LaunchDaemons/com.logga.client.daemon.service.plist"
+end
--- Casks/logga.rb
+++ Casks/logga.rb
@@ -1,34 +1,26 @@
-cask "logga-daemon" do
-  version "1.0.5"
-  sha256 "ae81d6230113af8a22e4b4dfe4e54745008c5c95e3cd62ec8fb937f810eb3513  logga-daemon-1.0.5.pkg"
+cask "logga" do
+  version "1.0.6"
+  sha256 "8d63ce7a88a13d44e5b1f685f6ee3ca269c6552f2351aa9d20f44b0dc451a867  logga-client-1.0.6.pkg"
 
-  url "https://storage.getlogga.com/logga-daemon-#{version}.pkg",
+  url "https://storage.getlogga.com/logga-client-#{version}.pkg",
     verified: "storage.getlogga.com"
-  name "logga daemon"
+  name "logga app"
   desc "Audit logging on modern macOS"
   homepage "https://getlogga.com/"
 
   depends_on macos: ">= :ventura"
 
-  daemon_path = "/Library/LaunchDaemons/com.logga.client.daemon.service.plist"
-
-  pkg "logga-daemon-#{version}.pkg"
-  uninstall pkgutil: "com.logga.daemon.*",
-            delete: [
-              "/Library/Application Support/Logga",
-              daemon_path
-            ],
-            launchctl: [
-              "com.logga.client.daemon.service"
-            ]
+  pkg "logga-client-#{version}.pkg"
+  uninstall pkgutil: "com.logga.client.*",
+            delete: "/Library/Application Support/Logga"
 
   uninstall_preflight do
-      system_command "/bin/launchctl", args: ["unload", "-w", daemon_path], sudo: true
+    system_command "/Applications/logga.app/Contents/MacOS/logga", args: ["unload"], sudo: false
   end
-  
+
   zap trash: [
     "/Library/Application Support/Logga",
   ]
 
-  caveats "To be able to use EndpointSecurity, "   "#{token} must be granted Full Disk Access under System Preferences → Security & Privacy → Privacy.\n\n"   "Starting the daemon: sudo launchctl load -w /Library/LaunchDaemons/com.logga.client.daemon.service.plist\n"   "Stopping the daemon: sudo launchctl load -w /Library/LaunchDaemons/com.logga.client.daemon.service.plist"
+  caveats "To be able to use EndpointSecurity, "   "#{token} must be granted Full Disk Access under System Preferences → Security & Privacy → Privacy"
 end
